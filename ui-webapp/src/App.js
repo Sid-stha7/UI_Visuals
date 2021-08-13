084b649793463c564cc1bfb2e20bf0d8201a35ad
Gr8Sushant <spokharel95@gmail.com>
2021-08-13 16:58:39 +0545

    white nav

--- ui-webapp/src/App.js
+++ ui-webapp/src/App.js
@@ -1,30 +1,14 @@
-import logo from './images/logo.png';
+import React, { component } from 'react';
 import './App.css';
+import Nav from './components/navbar.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Nav, Navbar, Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <Navbar bg="green" expand="lg" sticky="top">
-        <Container>
-          {/* <Navbar.Brand href="#home"><img src={logo} className ="logo" alt="logo"/></Navbar.Brand> */}
-          <img src={logo} className ="logo" alt="logo"/>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home" className=" text-light">Home</Nav.Link>
-              <Nav.Link href="#link" className=" text-light">About</Nav.Link>
-              <Nav.Link href="#link" className=" text-light">Team</Nav.Link>
-              <Nav.Link href="#link" className=" text-light">Events</Nav.Link>
-              <Nav.Link href="#link" className=" text-light">Contacts</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Nav/>
     </div>
   );
 }
-
 
 export default App;
