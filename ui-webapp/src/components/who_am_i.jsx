098bbef8fe0e8706fc7ae1e9259eb6ae5ee7e68b
Siddhartha (2049848) <np03A190145@heraldcollege.edu.np>
2021-08-21 17:13:10 +0545

    who are we components finalized

---
+++ ui-webapp/src/components/who_am_i.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class Portfolio extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div class="break"></div>
+
+        <div class="container">
+          <div class="row align-items-center justify-content-md-center">
+            <div class="col-lg-6 col-12 " id="port-main">
+              <h2 id="port-head">Who are we ?</h2>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+              ullamcorper condimentum ultrices. Cras euismod ornare laoreet.
+              Quisque vel efficitur quam, eu molestie odio. In hac habitasse
+              platea dictumst. Integer diam sapien.
+            </div>
+
+            <div class="col-lg-6 col-12  " id="image_div">
+              <div id="image-area"></div>
+            </div>
+          </div>
+          <div class="line"></div>
+
+          <div class="row align-items-center">
+            <div class="col-md ">
+              <span class="sub-head">LOREM IPSUM DOLOR</span>
+              <div class="col-md" id="desc-items">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                ullamcorper condimentum ultrices. Cras euismod ornare laoreet.
+              </div>
+            </div>
+
+            <div class="col-md ">
+              <span class="sub-head">LOREM IPSUM DOLOR</span>
+              <div class="col-md" id="desc-items">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                ullamcorper condimentum ultrices. Cras euismod ornare laoreet.
+              </div>
+            </div>
+            <div class="col-md ">
+              <span class="sub-head">LOREM IPSUM DOLOR</span>
+              <div class="col-md">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+                ullamcorper condimentum ultrices. Cras euismod ornare laoreet.
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Portfolio;
