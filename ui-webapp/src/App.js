5b8f6a0df8ac8560ff937593ae98662fb2d4f5bd
rabinsraizeee <np03s200004@heraldcollege.edu.np>
2021-08-24 21:20:04 +0545

    type(about):

--- ui-webapp/src/App.js
+++ ui-webapp/src/App.js
@@ -5,11 +5,15 @@ import NavComponent from './components/Navbar/navbar';
 import TestSlider from './components/Slidebar/slider';
 import Footer from "./components/Footer/Footer"
 import Portfolio from "./components/WHO_AM_I/Portfolio"
+import About from "./components/About/about"
+  
+  
 function App() {
   return (
     <div>
       <NavComponent />
-      <TestSlider/>
+      <TestSlider />
+      <About/>
       <Portfolio/>
       <Footer/>
       
