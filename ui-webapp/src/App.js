15fee4eda69dfaf552d197e5e90a671b8678b0da
rohan <rohansayami@gmail.com>
2021-08-26 15:38:08 +0545

    added frontend for testimoinals section

--- ui-webapp/src/App.js
+++ ui-webapp/src/App.js
@@ -7,6 +7,7 @@ import Footer from "./components/Footer/Footer"
 import Portfolio from "./components/WHO_AM_I/Portfolio"
 import About from "./components/About/about"
 import Contact_component from './components/Contact/contacts';
+import Testimonials from './components/Testimonials/Testimonials';
   
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About/>
       <Portfolio/>
       <Contact_component />
+      <Testimonials />
       <Footer/>
       
     </div>
