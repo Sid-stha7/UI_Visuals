1879165bec433f09880be4f5f4a814d1e592db05
Gr8Sushant <spokharel95@gmail.com>
2021-08-28 11:27:17 +0545

    banner added

--- ui-webapp/src/App.js
+++ ui-webapp/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import NavComponent from './components/Navbar/navbar';
 import TestSlider from './components/Slidebar/slider';
 import Footer from "./components/Footer/Footer"
 import Portfolio from "./components/WHO_AM_I/Portfolio"
 import About from "./components/About/about"
 import Contact_component from './components/Contact/contacts';
+import Banner from './components/Slidebar/Banner';
   
 function App() {
   return (
     <div>
       <NavComponent />
-      <TestSlider />
+      {/* <TestSlider /> */}
+      <Banner/>
       <About/>
       <Portfolio/>
       <Contact_component />
